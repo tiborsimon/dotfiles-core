316f51a1e51ec0329763c572438813193683fed7
Tibor Simon <tibor@tiborsimon.io>
2022-12-13 20:56:12 +0100

    env: proper terminal name handling

--- modules/shell/fish/config/generated/env_script.fish
+++ modules/shell/fish/config/generated/env_script.fish
@@ -1,9 +1,9 @@
-# Env script generated at Sun Aug 21 21:19:37 CEST 2022
+# Env script generated at Fri Dec  2 21:07:15 CET 2022
 
 # Collected environment variables:
 set --global --export 'EDITOR' 'nvim'
 set --global --export 'MANPAGER' 'sh -c "col -bx | bat -l man -p"'
 set --global --export 'PIPX_DEFAULT_PYTHON' '/Users/tibor/.pyenv/shims/python'
 set --global --export 'RIPGREP_CONFIG_PATH' '/Users/tibor/.config/ripgrep/ripgreprc'
-set --global --export 'TERM' 'alacritty'
+set --global --export 'TERM' 'screen-256color'
 set --global --export 'VIRTUALFISH_HOME' '/Users/tibor/.virtualenvs'
